77a7394cfa1b3aae3c98d08527a553aa6560f738
Leandro Araújo <leandro.arauj047.edu@gmail.com>
2023-10-18 10:18:09 -0300

    Adicionando

--- Aprendendo3/Praticando-para-prova-2.R
+++ Aprendendo3/Praticando-para-prova-2.R
@@ -1,3 +1,5 @@
+install.packages("readxl")
+
 library(readxl)
 
 1)
@@ -40,15 +42,69 @@ TukeyHSD(result)
 
 Resp: A fábrica de Seatle e a do Dallas são significativamente diferentes.
 
+3)
+
+jacare <- read_xlsx("jacare.xlsx")
+
+jacare
+
+result <- aov(jacare$Quantidade ~ factor(jacare$Local))
+anova(result)
+
+resp: Como Pr ( > F ) *  rejeitar H଴, ou seja, que existe diferenças
+entre os grupos (tratamentos).
+
+TukeyHSD(result)
+
+Resp: Considerando o nível de significância de 1%, os locais L3-L1, L3-L2, são significativamente diferentes.
+
+
 4) 
 
-url <- ("C:/Users/Windows/Documents/GitHub/Treinando-R/Aprendendo3/amostra.csv")
+x <- c(2.0, 2.0, 2.0, 4.0, 4.0, 4.0, 6.0, 6.0, 6.0, 8.0, 8.0, 8.0, 10.0, 10.0, 10.0)
+y <- c(2.1, 1.8, 1.9, 4.5, 4.2, 4.0, 6.2, 6.0, 6.5, 8.2, 7.8, 7.7, 9.6, 10.0, 10.1)
+tabela <- data.frame(x, y)
+tabela
 
-Reg = read.table(file = "C:/Users/Windows/Documents/GitHub/Treinando-R/Aprendendo3/amostra.csv", 
-                 header = TRUE, sep = ";", dec = ",") 
+plot(tabela$x, tabela$y)
+
+reglinear <- lm(data = tabela, formula = tabela$y ~ tabela$x)
+abline(reglinear)
+summary(reglinear)
+
+correlacao = cor(tabela$x,tabela$y)
+correlacao
+Determinacao = 0.9922
+
+y = 0.98*x + 0.16
+
+f(5) = 0.98 * 5 + 0.16
+f(12) = 0.98 * 12 + 0.16
+
+5)
+
+dados <- read_xlsx("dados.xlsx")
+dados
+
+plot(dados$x, dados$y)
+
+reglinear <- lm(data = dados, formula = dados$y ~ dados$x)
+abline(reglinear)
+summary(reglinear)
+
+R2 = 0.926
+
+#correlacao = sqrt(R2)
+correlacao = cor(dados$x,dados$y)
+
+correlacao
+
+r2 = correlacao ^2
+r2
 
 
 
+y = 0.9553*x + 9.8757
 
 
 
